4cf1b2d92e7428d093c4284078d69af24b545792
Smita Shinde <101444257+smita20BCS4643@users.noreply.github.com>
2022-06-17 00:38:35 +0530

    Update Insert_delete.java

--- 2.2 Insert_delete/src/smita/Insert_delete.java
+++ 2.2 Insert_delete/src/smita/Insert_delete.java
@@ -5,29 +5,29 @@ import java.util.*;
 public class Insert_delete {
 	public static void main(String[] args) {
 		List<String> liststring = new ArrayList<String>();
-// Inserting the elements in Arraylist
+                // Inserting the elements in Arraylist
 		liststring.add("Smita");
 		liststring.add("Dhruva");
 		liststring.add("Dhanashree");
 		liststring.add("Sushant");
 
 		System.out.println("|== Smita shinde   UID =20BCS4643 ==|\n");
-//		Displaying 
+                //		Displaying 
 		System.out.println("Printing The ArrayList is : " + liststring);
 
-// 		Searching
+                // 		Searching
 		System.out.println("\nSearching The Items in ArrayList is : ");
 		if (liststring.contains("IOT")) {
 			System.out.println("Item Found");
 		} else {
 			System.out.println("Item Not Found");
 		}
-//		Removing or Deleting
+                //		Removing or Deleting
 		System.out.println("\nRemoving The Element From ArrayList is : " + liststring.remove(0));
 		
 		System.out.println("\nAfter Removing The Element From Arraylist is :" + liststring);
 
-//		Retrieving 		
+                 //		Retrieving 		
 		System.out.println("\nRetrieving the element From ArrayList : ");
 		
 		String element = liststring.get(1); // retrieving the element
