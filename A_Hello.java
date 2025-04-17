490161caf88f724866cbfcb17547d4762859c0e4
Smita Shinde <101444257+smita20BCS4643@users.noreply.github.com>
2025-04-17 10:46:19 +0530

    revision

---
+++ A_Hello.java
@@ -0,0 +1,20 @@
+
+package smita;
+
+
+public class A_Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hwllo");
+		String Name = "SMita";
+		int age = 40;
+		
+		System.out.print(Name +" "+age);
+
+	}
+
+}
